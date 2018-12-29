1760f3645824f4730d98f99dace4b82578fb63b5
Teerasej <teerasej@gmail.com>
2018-12-29 11:44:59 +0700

    before implement service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  
+  contacts = [];
+
+  constructor(private http: HttpClient, private navCtrl: NavController){
+  }
+
+  ngOnInit(): void {
+    this.http
+        .get("https://randomuser.me/api/?results=20")
+        .subscribe((data:any) => {
+            console.log(data);
+            this.contacts = data.results;
+        });
+      
+  }
+
+  viewDetail(item){
+    this.navCtrl.navigateForward('/detail');
+  }
 
 }
